29ccf1d24e766741b210fa30cba7b7c3d4ea29a5
peyro <peyrovskaaa@gmail.com>
2025-03-12 17:05:13 +0100

    Update mojaDrugaAplikacja.java to perform basic arithmetic operations and prompt user for input

--- src/mojaDrugaAplikacja.java
+++ src/mojaDrugaAplikacja.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class mojaDrugaAplikacja {
     public static void main(String[] args) {
+        double x = 10; // utworzenie zmiennej typu double i przypisanie jej wartości 10
+        double y = 2;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter your first name:");
-        String firstname = scanner.nextLine();
-        System.out.println("Please enter your surname:");
-        String surname = scanner.nextLine();
+        var result = x + y;
+        System.out.println("x + y = " + result);
 
-        scanner.close();
+        result = x - y;
+        System.out.println("x - y = " + result);
 
-        System.out.println("Welcome " + firstname + " " + surname);
+        result = x * y;
+        System.out.println("x * y = " + result);
+
+        result = x / y;
+        System.out.println("x / y = " + result);
+
+        result = x % y;
+        System.out.println("x % y = " + result);
+
+        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
+        double first = scanner.nextDouble(); // próba o wpisanie wartości typu double
+        double second = scanner.nextDouble();
+
+        System.out.println("x + y = " + (first + second));
 
     }
 }
