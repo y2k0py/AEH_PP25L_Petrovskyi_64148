b399fc542246f55c2add2fda5ad6b2ec688f536d
peyro <peyrovskaaa@gmail.com>
2025-03-21 13:17:37 +0100

    Update mojaTrzeciaAplikacja.java to enhance user input handling for days and temperature conversions

--- src/mojaTrzeciaAplikacja.java
+++ src/mojaTrzeciaAplikacja.java
@@ -5,55 +5,33 @@ public class mojaTrzeciaAplikacja {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj proszę liczbę większą od 100:");
+        while (true) {
+            System.out.print("Podaj liczbę dni (wpisz wartość niedodatnią, aby zakończyć): ");
+            int days = scanner.nextInt();
 
-        //pętla WHILE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/while.png)
-        var number1 = scanner.nextDouble();
-        while (number1 <= 100) {
-            System.out.println("Liczba nie jest większa od 100, podaj jeszcze raz:");
-            number1 = scanner.nextDouble();
-        }
-        System.out.println("Dziękuję! Podałeś liczbę: " + number1);
-
-        //pętla DO..WHILE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/do-while.png)
-        double number2;
-        do {
-            System.out.println("Podaj proszę liczbę większą od 200:");
-            number2 = scanner.nextDouble();
-        } while (number2 <= 200);
-        System.out.println("Dziękuję! Podałeś liczbę: " + number2);
-
-        //pętla FOR (https://introcs.cs.princeton.edu/java/11cheatsheet/images/for.png)
-        int wynik = 0;
-        for (var i = 1; i <= 10; i++) {
-            wynik = wynik + i;
-            System.out.println("Przebieg numer " + i + " w pętli for, a zmienna wynik = " + wynik);
-        }
-
-        //instrukcje warunkowe IF..ELSE (https://introcs.cs.princeton.edu/java/11cheatsheet/images/if.png)
-        System.out.println("Podaj proszę liczbę x: ");
-        var x = scanner.nextDouble();
-        System.out.println("Podaj proszę liczbę y: ");
-        var y = scanner.nextDouble();
-
-        if(x > y){
-            System.out.println("x jest większe od y");
-        } else if (x < y) {
-            System.out.println("x jest mniejsze od y");
-        } else {
-            System.out.println("x jest równe y");
-        }
-
-        //zakończenie programu za pomocą wpisania odpowiedniego klawisza
-        while(true)
-        {
-            System.out.println("Wpisz wartość -1 żeby wyjść z programu");
-            var input = scanner.nextDouble();
-            if(input == -1){
-                System.out.println("Wyjście...");
+            if (days <= 0) {
                 break;
             }
+
+            int weeks = days / 7;
+            int remainingDays = days % 7;
+            System.out.println(days + " dni to " + weeks + " tygodnie i " + remainingDays + " dni.");
         }
+
+        while (true) {
+            System.out.print("Podaj temperaturę w Fahrenheitach (wpisz -1, aby zakończyć): ");
+            double fahrenheit = scanner.nextDouble();
+
+            if (fahrenheit == -1) {
+                break;
+            }
+
+            double celsius = (fahrenheit - 32) / 1.8;
+            double kelvin = celsius + 273.16;
+
+            System.out.printf("Fahrenheit: %.2f, Celsjusz: %.2f, Kelwin: %.2f\n", fahrenheit, celsius, kelvin);
+        }
+
         scanner.close();
     }
 }
