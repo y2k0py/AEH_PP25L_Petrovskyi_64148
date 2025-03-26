ef30f904461a0c66bb5f0b3c4d77e24edd0ea5c7
peyro <peyrovskaaa@gmail.com>
2025-03-26 17:03:28 +0100

    Add mojaCzwartaAplikacja.java to calculate the sum of squares within user-defined limits

---
+++ src/mojaCzwartaAplikacja.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class mojaCzwartaAplikacja {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.print("Enter lower and upper integer limits: ");
+            int lowerLimit = scanner.nextInt();
+            int upperLimit = scanner.nextInt();
+
+            if (lowerLimit >= upperLimit) {
+                scanner.close();
+                break;
+            }
+
+            int sum = 0;
+            for (int i = lowerLimit; i <= upperLimit; i++) {
+                sum += i * i;
+            }
+
+            System.out.println("The sum of the squares from " + lowerLimit * lowerLimit + " to " + upperLimit * upperLimit + " is " + sum);
+
+
+        }
+    }
+
+
+}
